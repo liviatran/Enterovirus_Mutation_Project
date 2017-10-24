9d0bb812503b37ae172def0096eb798a27e518ca
liviatran <tran.livia@berkeley.edu>
2017-10-24 12:46:58 -0700

    latest update

--- enterovirus.R
+++ enterovirus.R
@@ -3,35 +3,45 @@ setwd("~/desktop")
 library(seqinr)
 library(ape)
 
-enteroseqs<-read.fasta("enteroAli.txt")
+enteroseqs<-read.fasta("enteroshort.txt")
+ealigned<-read.alignment("enteroshort.txt", format="fasta")
 
 #gets DNA into matrix form
-enteromatrix<-read.dna("enteroAli.txt", format="fasta", as.character = TRUE)
+enteromatrix<-read.dna("enterodata.txt", format="fasta", as.character = TRUE)
 
 #gets WT NT for each DNA position 
-cons<-consensus(enteromatrix)
+cons<-seqinr::consensus(ealigned)
 
 #counts number of sequences with consensus NT
-numCons<-length(which(enteromatrix[,1]==cons[1]))
+numCons<-length(which(enteromatrix[]==cons[]))
 
 transition<-function(basepair){
   #basepair<-("A", "C", "T", "G"),
-  if(basepair=="A") {return("G")}
-  if(basepair=="G") {return ("A")}
-  if(basepair=="T") {return ("C")}
-  if(basepair=="C") {return ("T")}}
+  if(basepair=="a") {return("g")}
+  if(basepair=="g") {return ("a")}
+  if(basepair=="t") {return ("c")}
+  if(basepair=="c") {return ("t")}
+  }
+
+nrow(enterodata)
+ncol(enterodata)
 
 #determines transition from consensus
 numTrans<-length(which(enteromatrix=="transition"))
-transition(cons)
 
+transAA<-c()
+for(i in 1:length(cons)){
+  trans_letter<-transition(cons[i])
+  transAA<-c(transAA,trans_letter)
+}
 
+mutatedAA<-translate(transAA)
 
 #factor code 
 bigAAchange=factor(c(), levels=c(0,1), labels=c())
 
 #creates an new variable to make an empty data frame with 891 rows
-num<-c(1:864)
+num<-c(1:891)
 
 #creates the data frame with 891 rows 
 enterodata<-data.frame(num)
@@ -45,11 +55,9 @@ enterodata$bigAAchange=""
 #puts WTNT in first 5 elements in row 2
 cons->enterodata[,2]
 WTAA->enterodata[,3]
+mutatedAA->enterodata[,4]
 
 WTAA<-translate(cons)
-
-
-
 
 View(enterodata)
 
