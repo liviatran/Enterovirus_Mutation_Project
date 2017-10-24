71e7960b95a2d52da0dc6beb817b0c2a36482bd9
liviatran <tran.livia@berkeley.edu>
2017-10-24 12:43:36 -0700

    thank u sensei Dwayne

--- enterovirus.R
+++ enterovirus.R
@@ -4,11 +4,14 @@ library(seqinr)
 library(ape)
 
 enteroseqs<-read.fasta("enteroAli.txt")
+
+#gets DNA into matrix form
 enteromatrix<-read.dna("enteroAli.txt", format="fasta", as.character = TRUE)
 
+#gets WT NT for each DNA position 
 cons<-consensus(enteromatrix)
 
-cons->enterodata[,2]
+#counts number of sequences with consensus NT
 numCons<-length(which(enteromatrix[,1]==cons[1]))
 
 transition<-function(basepair){
@@ -18,9 +21,14 @@ transition<-function(basepair){
   if(basepair=="T") {return ("C")}
   if(basepair=="C") {return ("T")}}
 
-numTrans<-length(which(enteromatrix[1]=="transition"))
+#determines transition from consensus
+numTrans<-length(which(enteromatrix=="transition"))
+transition(cons)
 
-numTrans
+
+
+#factor code 
+bigAAchange=factor(c(), levels=c(0,1), labels=c())
 
 #creates an new variable to make an empty data frame with 891 rows
 num<-c(1:864)
@@ -30,28 +38,23 @@ enterodata<-data.frame(num)
 
 #creates 2 new columns 
 enterodata$WtNT=""
-enterodata$freq=""
+enterodata$WTAA=""
+enterodata$MutAA=""
+enterodata$bigAAchange=""
+
+#puts WTNT in first 5 elements in row 2
+cons->enterodata[,2]
+WTAA->enterodata[,3]
+
+WTAA<-translate(cons)
 
 
-#all elements are 891 in length
-element1<-enterodata[[1]]
-element1[1]
+
 
 View(enterodata)
 
-##Substr is a way to look at any range of positions of an element. This first line calls
-# for the first position of the first element. However, "c" should not be the first
-# position here (look at position 2 and 3 too). The first real nuc isn't shown until
-# position 4. This is something that will need to be fixed to properly call nucs
-substr(enterodata[1], 1, 1)
-substr(enterodata[1], 2, 2)
-substr(enterodata[1], 3, 3)
-substr(enterodata[1], 4, 4)
 
-##Table allows us to see the distribution of possibilities at a position in all of the
-# elements. Positons 4 & 9 are the first and second nucletide positons. Most frequent!
-table(substr(enterodata, 4, 4))
-table(substr(enterodata, 9, 9))
+
 
 
 
