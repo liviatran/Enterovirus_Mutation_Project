da222c844b4781c4af155f4acf2c28077293277a
deshawnhopson <deshawnhopson@gmail.com>
2017-10-25 23:07:27 -0700

    Our road so far

--- enterovirus.R
+++ enterovirus.R
@@ -3,11 +3,14 @@ PracticeData<-read.csv("OverviewSelCoeff_BachelerFilter.csv")
 library(seqinr)
 library(ape)
 
-#First we'll start the data frame
+#First we'll create the data frame
 Pos<-c(1:891)
-EnteroData<-data.frame(Pos)
-EnteroData$WtNt=""
-EnteroData$TrNtFreq=""
+enterodata<-data.frame(Pos)
+enterodata$WtNt=""
+enterodata$TrNtFreq=""
+enterodata$WTAA=""
+enterodata$MUTAA=""
+enterodata$bigAAchange=""
 
 #Read in shortened entero data
 enteroseqs<-read.fasta("enteroshort.txt")
@@ -21,8 +24,8 @@ enteromatrix<-read.dna("enteroshort.txt", format="fasta", as.character = TRUE)
 #Gets WTnt for each nt position
 cons<-seqinr::consensus(enteroaligned)
 
-#Insert concensus nt into dataframe
-EnteroData$WtNt=cons
+#Insert concensus into dataframe
+enterodata$WtNt=cons
 
 #Function to return transition mutation
 transition<-function(basepair){
@@ -33,50 +36,26 @@ transition<-function(basepair){
   if(basepair=="c") {return ("t")}
   }
 
-#Loop to insert freq of Transition mutations
-for(i in 1:nrow(EnteroData)){
-  EnteroData$TrNtFreq[i]<-length(which(enteromatrix[,i]==transition(cons[i])))/
+#Loop to insert the freq of transition mutations
+for(i in 1:nrow(enterodata)){
+  enterodata$TrNtFreq[i]<-length(which(enteromatrix[,i]==transition(cons[i])))/
     (nrow(enteromatrix))
-}
+  }
   
-#Loop to 
-transAA<-c()
-for(i in 1:length(cons)){
-  trans_letter<-transition(cons[i])
-  transAA<-c(transAA,trans_letter)
-}
-
-#
-mutatedAA<-translate(transAA)
-mutatedAA
-
-#factor code 
-bigAAchange=factor(c(), levels=c(0,1), labels=c())
-
-#creates 2 new columns 
-enterodata$WtNT=""
-enterodata$WTAA=""
-enterodata$MutAA=""
-enterodata$bigAAchange=""
-
-#puts WTNT in first 5 elements in row 2
-cons->enterodata[,2]
-WTAA->enterodata[,3]
-mutatedAA->enterodata[,4]
-
+#Translate the consensus to AAs
 translate(cons) -> wildAA
 
+#Insert the translated consensus into data frame
 count=0
 for(i in 1:length(wildAA)) {
-  count= count+1
+  count = count+1
   enterodata[count,]$WTAA=wildAA[i]
   count=count+1
   enterodata[count,]$WTAA=wildAA[i]
   count=count+1
   enterodata[count,]$WTAA=wildAA[i]
   }
-  
-  
 
 
-View(enterodata)
+
+
